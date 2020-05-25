3772a7081e57f30af65cd55666cfb020225e3e68
sunhengjava <sunhengjava@163.com>
2020-05-25 09:23:11 +0800

    sunheng修改了代码5/25

--- zsgc/oa_user_manager/oa_user_cunsumer_manager1/src/main/java/com/zb/UserConsumerTest.java
+++ zsgc/oa_user_manager/oa_user_cunsumer_manager1/src/main/java/com/zb/UserConsumerTest.java
@@ -2,7 +2,9 @@ package com.zb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//我修改了代码
+//hhhhh
+//孙合格
 @SpringBootApplication
 public class UserConsumerTest {
     public static void main(String[] args) {
