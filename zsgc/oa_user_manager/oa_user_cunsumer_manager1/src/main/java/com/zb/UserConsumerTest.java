04024c58f20f095a07808d26ceb455e13cbfec5d
sunhengjava <53298480+sunhengjava@users.noreply.github.com>
2020-05-25 09:33:12 +0800

    Update UserConsumerTest.java

--- zsgc/oa_user_manager/oa_user_cunsumer_manager1/src/main/java/com/zb/UserConsumerTest.java
+++ zsgc/oa_user_manager/oa_user_cunsumer_manager1/src/main/java/com/zb/UserConsumerTest.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //我修改了代码
 //hhhhh
 //孙合格
+//平台上修改，修改成功
 @SpringBootApplication
 public class UserConsumerTest {
     public static void main(String[] args) {
